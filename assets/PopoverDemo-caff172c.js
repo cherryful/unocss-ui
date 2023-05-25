5f0204e8f33ee81edad5bc7ac95a143d8d915b8e
szluyu99 <szluyu99@users.noreply.github.com>
2023-05-25 02:22:48 +0000

    deploy: b1978070b2784a0572bf174e27e96dcaee64949e

---
+++ assets/PopoverDemo-caff172c.js
@@ -0,0 +1,50 @@
+import{r as b,d as T,k as e,a as h,y as t,V as p,A as l,t as f,b as n,c as m,i as _,F as P,l as c}from"./index-811f0df6.js";import{_ as k,P as $}from"./Playground-740e0377.js";import{_ as v}from"./Sample.vue_vue_type_script_setup_true_lang-b0022a58.js";const w=n("div",{class:"px-4 py-2"}," The content of the UPopover. ",-1),A={class:"flex gap-3"},B={class:"flex flex-wrap items-center gap-2"},C=n("div",{class:"w-16"}," position ",-1),N={class:"flex flex-wrap items-center gap-2"},D=n("div",{class:"w-16"}," trigger ",-1),F={class:"flex gap-4"},O=n("div",{class:"px-4 py-2"}," The content of the UPopover. ",-1),R=n("div",{class:"px-4 py-2"}," The content of the UPopover. ",-1),W={class:"flex gap-4"},E=n("div",{class:"px-4 py-2"}," The content of the Popover. ",-1),L=n("div",{class:"px-4 py-2"}," The content of the Popover. ",-1),S=n("div",{class:"px-4 py-2"}," The content of the Popover. ",-1),Y=n("div",{class:"px-4 py-2"}," The content of the Popover. ",-1),j=n("div",{class:"px-4 py-2"}," The content of the Popover. ",-1),q=n("div",{class:"px-4 py-2"}," You can close the popover by clicking inside. ",-1),z=n("div",{class:"px-4 py-2"}," When hover, you need to click outside to close it. ",-1),G=n("div",{class:"px-4 py-2"}," When hover, you can use the mouse to access this panel. ",-1),H=n("div",{class:"px-4 py-2"}," The content of the Popover. ",-1),Q={__name:"PopoverDemo",setup(I){const s=b({position:"auto",trigger:"click",dissmissable:!1,sustain:!1,hoverAlive:!1,event:!1}),r={trigger:[`<UPopover trigger="hover">
+  hover
+  <template #content>
+    <div class="px-4 py-2">
+      The content of the UPopover.
+    </div>
+  </template>
+</UPopover>`,`<UPopover trigger="click">
+  click
+  <template #content>
+    <div class="px-4 py-2">
+      The content of the UPopover.
+    </div>
+  </template>
+</UPopover>`],position:[`<UPopover trigger="hover">
+  auto
+  <template #content>
+    <div class="px-4 py-2">
+      The content of the Popover.
+    </div>
+  </template>
+</UPopover>`,`<UPopover trigger="hover" position="left">
+  left
+  <template #content>
+    <div class="px-4 py-2">
+      The content of the Popover.
+    </div>
+  </template>
+</UPopover>`,`<UPopover trigger="hover" position="right">
+  right
+  <template #content>
+    <div class="px-4 py-2">
+      The content of the Popover.
+    </div>
+  </template>
+</UPopover>`,`<UPopover trigger="hover" position="top">
+  top
+  <template #content>
+    <div class="px-4 py-2">
+      The content of the Popover.
+    </div>
+  </template>
+</UPopover>`,`<UPopover trigger="hover" position="bottom">
+  bottom
+  <template #content>
+    <div class="px-4 py-2">
+      The content of the Popover.
+    </div>
+  </template>
+</UPopover>`],dissmissable:[],sustain:[],hoverAlive:[],events:[]},U=["auto","left","right","top","bottom"],y=["click","hover"];return(d,i)=>{const x=c("UButton"),u=c("UCheckbox"),g=c("URadio"),V=c("UToast");return h(),T(k,null,{default:e(()=>[t($,null,{preview:e(()=>[t(p,{position:s.value.position,trigger:s.value.trigger,dissmissable:s.value.dissmissable,sustain:s.value.sustain,"hover-alive":s.value.hoverAlive,onOpen:i[0]||(i[0]=o=>{var a;return s.value.event&&((a=d.$refs.toast)==null?void 0:a.info("open"))}),onClose:i[1]||(i[1]=o=>{var a;return s.value.event&&((a=d.$refs.toast)==null?void 0:a.info("close"))})},{content:e(()=>[w]),default:e(()=>[t(x,null,{default:e(()=>[l(f(s.value.trigger),1)]),_:1})]),_:1},8,["position","trigger","dissmissable","sustain","hover-alive"])]),props:e(()=>[n("div",A,[t(u,{modelValue:s.value.dissmissable,"onUpdate:modelValue":i[2]||(i[2]=o=>s.value.dissmissable=o)},{default:e(()=>[l(" dissmissable ")]),_:1},8,["modelValue"]),t(u,{modelValue:s.value.sustain,"onUpdate:modelValue":i[3]||(i[3]=o=>s.value.sustain=o)},{default:e(()=>[l(" sustain ")]),_:1},8,["modelValue"]),t(u,{modelValue:s.value.hoverAlive,"onUpdate:modelValue":i[4]||(i[4]=o=>s.value.hoverAlive=o)},{default:e(()=>[l(" hover-alive ")]),_:1},8,["modelValue"]),t(u,{modelValue:s.value.event,"onUpdate:modelValue":i[5]||(i[5]=o=>s.value.event=o)},{default:e(()=>[l(" with event ")]),_:1},8,["modelValue"])]),n("div",B,[C,(h(),m(P,null,_(U,o=>t(g,{key:o,modelValue:s.value.position,"onUpdate:modelValue":i[6]||(i[6]=a=>s.value.position=a),type:o,value:o},{default:e(()=>[l(f(o),1)]),_:2},1032,["modelValue","type","value"])),64))]),n("div",N,[D,(h(),m(P,null,_(y,o=>t(g,{key:o,modelValue:s.value.trigger,"onUpdate:modelValue":i[7]||(i[7]=a=>s.value.trigger=a),type:o,value:o},{default:e(()=>[l(f(o),1)]),_:2},1032,["modelValue","type","value"])),64))])]),_:1}),t(v,{title:"trigger",snippets:r.trigger},{default:e(()=>[n("div",F,[t(p,{trigger:"hover"},{content:e(()=>[O]),default:e(()=>[l(" hover ")]),_:1}),t(p,{trigger:"click"},{content:e(()=>[R]),default:e(()=>[l(" click ")]),_:1})])]),_:1},8,["snippets"]),t(v,{title:"position",snippets:r.position},{default:e(()=>[n("div",W,[t(p,{trigger:"hover"},{content:e(()=>[E]),default:e(()=>[l(" auto ")]),_:1}),t(p,{trigger:"hover",position:"left"},{content:e(()=>[L]),default:e(()=>[l(" left ")]),_:1}),t(p,{trigger:"hover",position:"right"},{content:e(()=>[S]),default:e(()=>[l(" right ")]),_:1}),t(p,{trigger:"hover",position:"top"},{content:e(()=>[Y]),default:e(()=>[l(" top ")]),_:1}),t(p,{trigger:"hover",position:"bottom"},{content:e(()=>[j]),default:e(()=>[l(" bottom ")]),_:1})])]),_:1},8,["snippets"]),t(v,{title:"dissmissable",snippets:r.dissmissable},{default:e(()=>[t(p,{trigger:"click",dissmissable:""},{content:e(()=>[q]),default:e(()=>[l(" dissmissable ")]),_:1})]),_:1},8,["snippets"]),t(v,{title:"sustain",snippets:r.sustain},{default:e(()=>[t(p,{trigger:"hover",sustain:""},{content:e(()=>[z]),default:e(()=>[l(" hover & sustain ")]),_:1})]),_:1},8,["snippets"]),t(v,{title:"hover-alive",snippets:r.sustain},{default:e(()=>[t(p,{trigger:"hover","hover-alive":""},{content:e(()=>[G]),default:e(()=>[l(" hover & hover-alive ")]),_:1})]),_:1},8,["snippets"]),t(v,{title:"events",snippets:r.events},{default:e(()=>[t(V,{ref:"toast"},null,512),t(p,{onOpen:i[8]||(i[8]=o=>{var a;return(a=d.$refs.toast)==null?void 0:a.info("open")}),onClose:i[9]||(i[9]=o=>{var a;return(a=d.$refs.toast)==null?void 0:a.info("close")})},{content:e(()=>[H]),default:e(()=>[l(" events ")]),_:1})]),_:1},8,["snippets"])]),_:1})}}};export{Q as default};
---
+++ assets/ToastDemo-06a5c679.js
@@ -0,0 +1,89 @@
+import{r as b,d as B,k as i,a as d,y as s,a0 as c,c as _,b as n,n as v,A as l,t as m,j as V,i as w,F as y,l as h}from"./index-811f0df6.js";import{_ as C,P as q}from"./Playground-740e0377.js";import{_ as p}from"./Sample.vue_vue_type_script_setup_true_lang-b0022a58.js";const N={key:0,class:"pointer-events-auto max-w-sm w-full overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"},A={class:"p-4"},I={class:"flex items-start"},P={class:"shrink-0"},D={class:"ml-3 w-0 flex-1 pt-0.5"},F={class:"text-sm font-medium text-gray-900"},R=n("p",{class:"mt-1 text-sm text-gray-500"}," Anyone with a link can now view this file. ",-1),j={class:"flex flex-wrap gap-2"},z={class:"flex flex-wrap items-center gap-2"},E=n("div",{class:"w-20"}," type ",-1),L={class:"flex flex-wrap items-center gap-2"},Q=n("div",{class:"w-20"}," position ",-1),S={class:"flex flex-wrap items-center gap-2"},G=n("div",{class:"w-20"}," align ",-1),H={class:"flex items-center gap-2"},J=n("div",{class:"w-20"}," content ",-1),K={class:"flex items-center"},M=n("div",{class:"w-20"}," timeout ",-1),O={class:"w-36"},W={class:"flex gap-4"},X={class:"pointer-events-auto w-full overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"},Y={class:"flex items-start p-4"},Z={class:"shrink-0"},tt={class:"ml-3 w-0 flex-1 pt-0.5"},et={class:"text-sm font-medium text-gray-900"},st=n("p",{class:"mt-1 text-sm text-gray-500"}," Anyone with a link can now view this file. ",-1),ot={class:"flex gap-4"},it={class:"flex gap-4"},nt={class:"flex gap-4"},lt={class:"flex gap-4"},ft={__name:"ToastDemo",setup(at){const f={base:[`<UToast ref="toast_basic" />
+<UButton @click="$refs.toast_basic?.show('success', 'It is simple to use the toast.')">
+  show
+</UButton>`],type:[`<UToast ref="toast_type" />
+
+<div class="space-x-4 space-y-3">
+  <UButton type="success" @click="$refs.toast_type?.success('This is the success toast.')">
+    success
+  </UButton>
+  <UButton type="info" @click="$refs.toast_type?.info('This is the info toast.')">
+    info
+  </UButton>
+  <UButton type="warning" @click="$refs.toast_type?.warning('This is the warning toast.')">
+    warning
+  </UButton>
+  <UButton type="error" @click="$refs.toast_type?.error('This is the error toast.')">
+    error
+  </UButton>
+</div>`],timeout:[`<UToast ref="toast_timeout" :timeout="500" />
+<UButton @click="$refs.toast_timeout?.info('This toast will disappear after 500ms.')">
+  500ms
+</UButton>`],queue:[`<UToast ref="toast_queue" :queue="false" />
+<UButton @click="$refs.toast_queue.success('This is the no queue toast.')">
+  no queue
+</UButton>`],custom:[`<UToast ref="toast_custom" v-slot="{ type }">
+  <div class="pointer-events-auto max-w-sm w-full overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+    <div class="p-4">
+      <div class="flex items-start">
+        <div class="shrink-0">
+          <div
+            class="h-6 w-6"
+            :class="{
+              'i-mdi:check-circle text-success-400': type === 'success',
+              'i-mdi:information-outline text-info-400': type === 'info',
+              'i-mdi:alert-outline text-warning-400': type === 'warning',
+              'i-mdi:alert-circle-outline text-error-400': type === 'error',
+            }"
+          />
+        </div>
+        <div class="ml-3 w-0 flex-1 pt-0.5">
+          <p class="text-sm font-medium text-gray-900">
+            This is the custom {{ type }}  toast.
+          </p>
+          <p class="mt-1 text-sm text-gray-500">
+            Anyone with a link can now view this file.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</UToast>
+<div class="space-x-4 space-y-3">
+  <UButton
+    type="success"
+    @click="$refs.toast_custom.success('This is the success toast.')"
+  >
+    success
+  </UButton>
+  <UButton
+    type="info"
+    @click="$refs.toast_custom.info('This is the info toast.')"
+  >
+    info
+  </UButton>
+  <UButton
+    type="warning"
+    @click="$refs.toast_custom.warning('This is the warning toast.')"
+  >
+    warning
+  </UButton>
+  <UButton
+    type="error"
+    @click="$refs.toast_custom.error('This is the error toast.')"
+  >
+    error
+  </UButton>
+</div>`],placement:[`<UToast ref="toast_top_left" position="top" align="left" />
+<UButton @click="$refs.toast_top_left?.success('This is the top left toast.')">
+  top-left
+</UButton>`,`<UToast ref="toast_top_right" position="top" align="right" />
+<UButton @click="$refs.toast_top_right?.info('This is the top right toast.')">
+  top-right
+</UButton>`,`<UToast ref="toast_bottom_left" position="bottom" align="left" />
+<UButton @click="$refs.toast_bottom_left?.warning('This is the bottom left toast.')">
+  bottom-left
+</UButton>`,`<UToast ref="toast_bottom_right" position="bottom" align="right" />
+<UButton @click="$refs.toast_bottom_right?.error('This is the bottom right toast.')">
+  bottom-right
+</UButton>`]},a=b({type:"success",timeout:2500,position:"top",align:"left",content:"This toast is very simple and easy to use.",queue:!0,custom:!1}),$=["success","info","warning","error"],T=["top","bottom"],x=["left","center","right"];return(r,e)=>{const u=h("UButton"),U=h("UCheckbox"),g=h("URadio"),k=h("UInput");return d(),B(C,null,{default:i(()=>[s(q,null,{preview:i(()=>[s(c,{ref:"toast",type:a.value.type,timeout:a.value.timeout,queue:a.value.queue,position:a.value.position,align:a.value.align},{default:i(({type:t})=>[a.value.custom?(d(),_("div",N,[n("div",A,[n("div",I,[n("div",P,[n("div",{class:v(["h-6 w-6",{"i-mdi:check-circle text-success-400":t==="success","i-mdi:information-outline text-info-400":t==="info","i-mdi:alert-outline text-warning-400":t==="warning","i-mdi:alert-circle-outline text-error-400":t==="error"}])},null,2)]),n("div",D,[n("p",F,[l(" This is the custom "),n("span",{class:v(["font-bold",{"text-success-400":t==="success","text-info-400":t==="info","text-warning-400":t==="warning","text-error-400":t==="error"}])},m(t),3),l(" toast. ")]),R])])])])):V("",!0)]),_:1},8,["type","timeout","queue","position","align"]),s(u,{onClick:e[0]||(e[0]=t=>r.$refs.toast.show(a.value.type,a.value.content))},{default:i(()=>[l(" show ")]),_:1})]),props:i(()=>[n("div",j,[s(U,{modelValue:a.value.queue,"onUpdate:modelValue":e[1]||(e[1]=t=>a.value.queue=t)},{default:i(()=>[l(" queue ")]),_:1},8,["modelValue"]),s(U,{modelValue:a.value.custom,"onUpdate:modelValue":e[2]||(e[2]=t=>a.value.custom=t)},{default:i(()=>[l(" custom content ")]),_:1},8,["modelValue"])]),n("div",z,[E,(d(),_(y,null,w($,t=>s(g,{key:t,modelValue:a.value.type,"onUpdate:modelValue":e[3]||(e[3]=o=>a.value.type=o),type:t,value:t},{default:i(()=>[l(m(t),1)]),_:2},1032,["modelValue","type","value"])),64))]),n("div",L,[Q,(d(),_(y,null,w(T,t=>s(g,{key:t,modelValue:a.value.position,"onUpdate:modelValue":e[4]||(e[4]=o=>a.value.position=o),type:t,value:t},{default:i(()=>[l(m(t),1)]),_:2},1032,["modelValue","type","value"])),64))]),n("div",S,[G,(d(),_(y,null,w(x,t=>s(g,{key:t,modelValue:a.value.align,"onUpdate:modelValue":e[5]||(e[5]=o=>a.value.align=o),type:t,value:t},{default:i(()=>[l(m(t),1)]),_:2},1032,["modelValue","type","value"])),64))]),n("div",H,[J,s(k,{modelValue:a.value.content,"onUpdate:modelValue":e[6]||(e[6]=t=>a.value.content=t),placeholder:"content",clearable:""},null,8,["modelValue"])]),n("div",K,[M,n("div",O,[s(k,{modelValue:a.value.timeout,"onUpdate:modelValue":e[7]||(e[7]=t=>a.value.timeout=t),placeholder:"timeout",type:"number"},null,8,["modelValue"])])])]),_:1}),s(p,{title:"Base",snippets:f.base},{default:i(()=>[s(c,{ref:"toast_basic"},null,512),s(u,{onClick:e[8]||(e[8]=t=>{var o;return(o=r.$refs.toast_basic)==null?void 0:o.show("success","It is simple to use the toast.")})},{default:i(()=>[l(" show ")]),_:1})]),_:1},8,["snippets"]),s(p,{title:"Type",snippets:f.type},{default:i(()=>[s(c,{ref:"toast_type"},null,512),n("div",W,[s(u,{type:"success",onClick:e[9]||(e[9]=t=>{var o;return(o=r.$refs.toast_type)==null?void 0:o.success("This is the success toast.")})},{default:i(()=>[l(" success ")]),_:1}),s(u,{type:"info",onClick:e[10]||(e[10]=t=>{var o;return(o=r.$refs.toast_type)==null?void 0:o.info("This is the info toast.")})},{default:i(()=>[l(" info ")]),_:1}),s(u,{type:"warning",onClick:e[11]||(e[11]=t=>{var o;return(o=r.$refs.toast_type)==null?void 0:o.warning("This is the warning toast.")})},{default:i(()=>[l(" warning ")]),_:1}),s(u,{type:"error",onClick:e[12]||(e[12]=t=>{var o;return(o=r.$refs.toast_type)==null?void 0:o.error("This is the error toast.")})},{default:i(()=>[l(" error ")]),_:1})])]),_:1},8,["snippets"]),s(p,{title:"Custom",snippets:f.custom},{default:i(()=>[s(c,{ref:"toast_custom"},{default:i(({type:t})=>[n("div",X,[n("div",Y,[n("div",Z,[n("div",{class:v(["h-6 w-6",{"i-mdi:check-circle text-success-400":t==="success","i-mdi:information-outline text-info-400":t==="info","i-mdi:alert-outline text-warning-400":t==="warning","i-mdi:alert-circle-outline text-error-400":t==="error"}])},null,2)]),n("div",tt,[n("p",et," This is the custom "+m(t)+" toast. ",1),st])])])]),_:1},512),n("div",ot,[s(u,{type:"success",onClick:e[13]||(e[13]=t=>r.$refs.toast_custom.success("This is the success toast."))},{default:i(()=>[l(" success ")]),_:1}),s(u,{type:"info",onClick:e[14]||(e[14]=t=>r.$refs.toast_custom.info("This is the info toast."))},{default:i(()=>[l(" info ")]),_:1}),s(u,{type:"warning",onClick:e[15]||(e[15]=t=>r.$refs.toast_custom.warning("This is the warning toast."))},{default:i(()=>[l(" warning ")]),_:1}),s(u,{type:"error",onClick:e[16]||(e[16]=t=>r.$refs.toast_custom.error("This is the error toast."))},{default:i(()=>[l(" error ")]),_:1})])]),_:1},8,["snippets"]),s(p,{title:"Timeout",snippets:f.timeout},{default:i(()=>[s(c,{ref:"toast_timeout",timeout:500},null,512),n("div",it,[s(u,{onClick:e[17]||(e[17]=t=>{var o;return(o=r.$refs.toast_timeout)==null?void 0:o.info("This toast will disappear after 500ms.")})},{default:i(()=>[l(" 500 ms ")]),_:1})])]),_:1},8,["snippets"]),s(p,{title:"No Queue",snippets:f.queue},{default:i(()=>[s(c,{ref:"toast_queue",queue:!1},null,512),n("div",nt,[s(u,{onClick:e[18]||(e[18]=t=>r.$refs.toast_queue.success("This is the no queue toast."))},{default:i(()=>[l(" no queue ")]),_:1})])]),_:1},8,["snippets"]),s(p,{title:"Placement",snippets:f.placement},{default:i(()=>[n("div",lt,[s(c,{ref:"toast_top_left",position:"top",align:"left"},null,512),s(u,{onClick:e[19]||(e[19]=t=>{var o;return(o=r.$refs.toast_top_left)==null?void 0:o.success("This is the top left toast.")})},{default:i(()=>[l(" top-left ")]),_:1}),s(c,{ref:"toast_top_right",position:"top",align:"right"},null,512),s(u,{onClick:e[20]||(e[20]=t=>{var o;return(o=r.$refs.toast_top_right)==null?void 0:o.info("This is the top right toast.")})},{default:i(()=>[l(" top-right ")]),_:1}),s(c,{ref:"toast_bottom_left",position:"bottom",align:"left"},null,512),s(u,{onClick:e[21]||(e[21]=t=>{var o;return(o=r.$refs.toast_bottom_left)==null?void 0:o.warning("This is the bottom left toast.")})},{default:i(()=>[l(" bottom-left ")]),_:1}),s(c,{ref:"toast_bottom_right",position:"bottom",align:"right"},null,512),s(u,{onClick:e[22]||(e[22]=t=>{var o;return(o=r.$refs.toast_bottom_right)==null?void 0:o.error("This is the bottom right toast.")})},{default:i(()=>[l(" bottom-right ")]),_:1})])]),_:1},8,["snippets"])]),_:1})}}};export{ft as default};
